0d3ba637fe20e39c10c9e41b356adeb13d111754
Daniel <danielzegers22o@gmail.com>
2022-12-27 04:28:53 -0300

    Repo name

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? '/your-github-repo-name/' : '',
+  assetPrefix: isProd ? '/mhw-builder/' : '',
   images: {
     loader: 'akamai',
     path: '',
