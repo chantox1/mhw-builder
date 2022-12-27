93f71996f82480edb19d101ef1361c97886b68d7
Daniel <danielzegers22o@gmail.com>
2022-12-27 03:32:49 -0300

    Update workflows

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  assetPrefix: isProd ? '/your-github-repo-name/' : '',
+  images: {
+    unoptimized: true,
+  },
+}
