cf1cc5c5369d6d42f0a6e46fbccea4b3e365bce4
Daniel <danielzegers22o@gmail.com>
2022-12-27 04:41:48 -0300

    Vercel test

--- next.config.js
+++ next.config.js
@@ -2,9 +2,8 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? '/mhw-builder/' : '',
   images: {
     loader: 'akamai',
-    path: '/mhw-builder/',
+    path: '',
   },
 }
