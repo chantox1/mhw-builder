17112138e1c46642bcc0cb8946dcff9200999042
chantox1 <danielzegers22o@gmail.com>
2023-01-03 20:08:41 -0300

    More robust skill effect format

--- data/IB/common/skill_bonus.js
+++ data/IB/common/skill_bonus.js
@@ -21,133 +21,181 @@ Classes:
 
 export const skill_bonus = {
     19: [ // Attack boost
-        {
-            class: 2, 
-            param: 1
-        },
-        {
-            class: 4,
-            param: 2
+        {   
+            effects: [
+            {
+                class: 2, 
+                param: 1
+            },
+            {
+                class: 4,
+                param: 2
+            }
+            ]
         }
     ],
     30: [ // Fire attack
-        {
-            class: 7,
-            param: 1,
-            extra: 0  // TODO: check these ele ids are right
-        },
-        {
-            class: 6,
-            param: 2,
-            extra: 0
+        {   
+            effects: [
+            {
+                class: 7, 
+                param: 1,
+                extra: 0
+            },
+            {
+                class: 6,
+                param: 2,
+                extra: 0
+            }
+            ]
         }
     ],
     31: [ // Water attack
-        {
-            class: 7,
-            param: 1,
-            extra: 1
-        },
-        {
-            class: 6,
-            param: 2,
-            extra: 1
+        {   
+            effects: [
+            {
+                class: 7, 
+                param: 1,
+                extra: 1
+            },
+            {
+                class: 6,
+                param: 2,
+                extra: 1
+            }
+            ]
         }
     ],
     32: [ // Ice attack
-        {
-            class: 7,
-            param: 1,
-            extra: 2
-        },
-        {
-            class: 6,
-            param: 2,
-            extra: 2
+        {   
+            effects: [
+            {
+                class: 7, 
+                param: 1,
+                extra: 2
+            },
+            {
+                class: 6,
+                param: 2,
+                extra: 2
+            }
+            ]
         }
     ],
     33: [ // Thunder attack
-        {
-            class: 7,
-            param: 1,
-            extra: 3
-        },
-        {
-            class: 6,
-            param: 2,
-            extra: 3
+        {   
+            effects: [
+            {
+                class: 7, 
+                param: 1,
+                extra: 3
+            },
+            {
+                class: 6,
+                param: 2,
+                extra: 3
+            }
+            ]
         }
     ],
     34: [ // Dragon attack
-        {
-            class: 7,
-            param: 1,
-            extra: 4
-        },
-        {
-            class: 6,
-            param: 2,
-            extra: 4
+        {   
+            effects: [
+            {
+                class: 7, 
+                param: 1,
+                extra: 4
+            },
+            {
+                class: 6,
+                param: 2,
+                extra: 4
+            }
+            ]
         }
     ],
     35: [ // Poison attack
-        {
-            class: 8,
-            param: 0,   // Not used
-            extra: 5
-        },
-        {
-            class: 6,
-            param: 1,
-            extra: 5
+        {   
+            effects: [
+            {
+                class: 8, 
+                param: 0,
+                extra: 5
+            },
+            {
+                class: 6,
+                param: 1,
+                extra: 5
+            }
+            ]
         }
     ],
     36: [ // Paralysis attack
-        {
-            class: 8,
-            param: 0,
-            extra: 6
-        },
-        {
-            class: 6,
-            param: 1,
-            extra: 6
+        {   
+            effects: [
+            {
+                class: 8, 
+                param: 0,
+                extra: 6
+            },
+            {
+                class: 6,
+                param: 1,
+                extra: 6
+            }
+            ]
         }
     ],
     37: [ // Sleep attack
-        {
-            class: 8,
-            param: 0,
-            extra: 7
-        },
-        {
-            class: 6,
-            param: 1,
-            extra: 7
+        {   
+            effects: [
+            {
+                class: 8, 
+                param: 0,
+                extra: 7
+            },
+            {
+                class: 6,
+                param: 1,
+                extra: 7
+            }
+            ]
         }
     ],
     38: [ // Blast attack
-        {
-            class: 8,
-            param: 0,
-            extra: 8
-        },
-        {
-            class: 6,
-            param: 1,
-            extra: 8
+        {   
+            effects: [
+            {
+                class: 8, 
+                param: 0,
+                extra: 8
+            },
+            {
+                class: 6,
+                param: 1,
+                extra: 8
+            }
+            ]
         }
     ],
     48: [ // Critical eye
-        {
-            class: 4,
-            param: 1
+        {   
+            effects: [
+            {
+                class: 4, 
+                param: 1
+            }
+            ]
         }
     ],
     49: [ // Critical boost
-        {
-            class: 5,
-            param: 1
+        {   
+            effects: [
+            {
+                class: 5, 
+                param: 1
+            }
+            ]
         }
-    ]
+    ],
 }
