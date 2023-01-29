fd7d9963838dfefcbaf735adb1dc54886741f6c9
chantox1 <danielzegers22o@gmail.com>
2023-01-29 02:42:56 -0300

    Deco colors via sprite
    Pending replacement in search_equip

--- components/slot_display.js
+++ components/slot_display.js
@@ -4,6 +4,34 @@ import { Card, CardMedia } from '@mui/material';
 import { Typography } from '@mui/material';
 import { ButtonBase } from '@mui/material';
 
+const Sprite = (props) => {
+  const { src, pos, crop, width, p=0 } = props;
+  const norm = (width-2*p)/crop[0]
+
+  return (
+    <Box
+      sx={{
+        ...props.sx,
+        width: width,
+        height: crop[1]*norm,
+        padding: p
+      }}
+    >
+      <Box
+        sx={{
+          width: crop[0],
+          height: crop[1],
+          backgroundImage: `url(${src})`,
+          backgroundPosition: `${pos[0]*-1}px ${pos[1]*-1}px`,
+          backgroundRepeat: 'no-repeat',
+          transform: `scale(${norm})`,
+          transformOrigin: '0 0',
+        }}
+      />
+    </Box>
+  )
+}
+
 export default function SlotDisplay(props) {
   const { data, main=false, slot, pos, sx, type, onClick } = props;
 
@@ -14,26 +42,37 @@ export default function SlotDisplay(props) {
         <ButtonBase sx={sx}
           onClick = {() => onClick({Mode: 1, Type: type, Pos: pos, Size: slot, Item: slot})}
         >
-          <Box display="flex" height="100%">
-            <Box
-              maxHeight={25}
-              component="img"
-              src={'/icon/Slot/' + slot + '.png'}
+          <Box display="flex">
+            <Sprite
+              src='/icon/Slot/gems-base.png'
+              pos={[33*(slot - 1),0]}
+              width={27}
+              crop={[33,30]}
             />
           </Box>
         </ButtonBase>
       )
     }
     else {
+      console.log("filled slot:")
+      console.log(slot.Deco)
       return (
         <ButtonBase sx={sx}
           onClick = {() => onClick({Mode: 1, Type: type, Pos: pos, Size: slot.Size, Item: slot})}
         >
-          <Box display="flex" height="100%">
-            <Box
-              maxHeight={25}
-              component="img"
-              src={'/icon/Slot/' + slot.Size + '.png'}
+          <Box display="flex">
+            <Sprite
+              src='/icon/Slot/gems-base.png'
+              pos={[33*(slot.Size - 1),30*(slot.Deco.Size)]}
+              width={27}
+              crop={[33,30]}
+            />
+            <Sprite
+              src='/icon/Slot/gems-over.png'
+              pos={[33*(slot.Size - 1),30*(slot.Deco.Color)]}
+              width={27}
+              crop={[33,30]}
+              sx={{position: 'absolute'}}
             />
             <Box alignSelf="center" ml={0.5}>
               <Typography noWrap> { data.decoString[slot.Deco.Name] } </Typography>
@@ -45,10 +84,11 @@ export default function SlotDisplay(props) {
   }
   return (
     <Box display="flex" sx={sx}>
-      <Box
-        maxHeight={25}
-        component="img"
-        src={'/icon/Slot/' + slot + '.png'}
+      <Sprite
+        src='/icon/Slot/gems-base.png'
+        pos={[33*(slot - 1),0]}
+        width={27}
+        crop={[33,30]}
       />
     </Box>
   )
