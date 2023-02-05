67104e2e024e8005fcdebdc2422c0508f8b7345c
chantox1 <danielzegers22o@gmail.com>
2023-02-05 03:38:00 -0300

    Update navbar & adjust tabulation

--- components/navbar.js
+++ components/navbar.js
@@ -1,40 +1,38 @@
 import * as React from 'react';
-import { AppBar, Toolbar, Link, Typography, Box, FormControl, InputLabel, OutlinedInput } from "@mui/material";
-import BuildCircleIcon from '@mui/icons-material/BuildCircle';
+import { AppBar, Toolbar, Container } from "@mui/material";
+import { Select, MenuItem } from '@mui/material';
+import { IconButton, Link } from '@mui/material';
+import { Typography } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
 import LanguageIcon from '@mui/icons-material/Language';
-import { Container } from "@mui/material";
-import { Grid } from "@mui/material";
-import Select from '@mui/material/Select';
-import { ButtonBase } from '@mui/material';
-import { MenuItem } from '@mui/material';
 
 export default function NavBar(props) {
-    const {IB=false, msg} = props
+  const {IB=false, msg} = props
 
-    return (
-        <AppBar position="static" elevation={0} sx={{mb: 1, borderBottom: 2, borderColor: 'text.secondary'}}>
-            <Container maxWidth="xl">
-                <Toolbar>
-                    <Link href="../..">
-                        <BuildCircleIcon sx={{mr:2}}/>
-                    </Link>
-                    <Typography variant="h4" flex={1}>
-                        { msg }
-                    </Typography>
-                    {IB &&
-                    <Select
-                        IconComponent={LanguageIcon}
-                        sx={{'.MuiOutlinedInput-notchedOutline': { border: 0 }}}
-                    >
-                        <MenuItem>
-                            <Link href="/../builder/IB/eng" color='text.primary'>English</Link>
-                        </MenuItem>
-                        <MenuItem>
-                            <Link href="/../builder/IB/jp" color='text.primary'>日本語</Link>
-                        </MenuItem>
-                    </Select>}
-                </Toolbar>
-            </Container>
-        </AppBar>
-    )
+  return (
+    <AppBar position="static" elevation={0} sx={{mb: 1, borderBottom: 1, borderColor: 'text.secondary'}}>
+      <Container maxWidth="xl">
+        <Toolbar variant='dense'>
+          <IconButton href="../.." edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <HomeIcon />
+          </IconButton>
+          <Typography alignSelf="center" variant="h5" flex={1}>
+            { msg }
+          </Typography>
+          {IB &&
+          <Select
+            IconComponent={LanguageIcon}
+            sx={{'.MuiOutlinedInput-notchedOutline': { border: 0 }}}
+          >
+            <MenuItem>
+              <Link href="/../builder/IB/eng" color='text.primary'>English</Link>
+            </MenuItem>
+            <MenuItem>
+              <Link href="/../builder/IB/jp" color='text.primary'>日本語</Link>
+            </MenuItem>
+          </Select>}
+        </Toolbar>
+      </Container>
+    </AppBar>
+  )
 }
--- pages/builder/index.js
+++ pages/builder/index.js
@@ -3,26 +3,26 @@ import { Box, Container } from '@mui/material';
 import { Card, CardActionArea, CardMedia } from '@mui/material';
 
 export default function Builder() {
-    return (
-            <Container maxWidth="sm" sx={{height: "80vh"}}>
-                <Box sx={{display: "flex", flexDirection: "column", height: "100%", justifyContent: "space-around"}}>
-                    <Card>
-                        <CardActionArea href="/builder/IB/eng">
-                            <CardMedia sx={{objectFit: "contain"}}
-                                component="img"
-                                image="/logo/icebornelogo.png"
-                            />
-                        </CardActionArea>
-                    </Card>
-                    <Card>
-                        <CardActionArea href="/builder/ICE">
-                            <CardMedia sx={{objectFit: "contain"}}
-                                component="img"
-                                image="/logo/icelogo.png"
-                            />
-                        </CardActionArea>
-                    </Card>
-                </Box>
-            </Container>
-    );
+  return (
+    <Container maxWidth="sm" sx={{height: "80vh"}}>
+      <Box sx={{display: "flex", flexDirection: "column", height: "100%", justifyContent: "space-around"}}>
+        <Card>
+          <CardActionArea href="/builder/IB/eng">
+            <CardMedia sx={{objectFit: "contain"}}
+              component="img"
+              image="/logo/icebornelogo.png"
+            />
+          </CardActionArea>
+        </Card>
+        <Card>
+          <CardActionArea href="/builder/ICE">
+            <CardMedia sx={{objectFit: "contain"}}
+              component="img"
+              image="/logo/icelogo.png"
+            />
+          </CardActionArea>
+        </Card>
+      </Box>
+    </Container>
+  );
 }
